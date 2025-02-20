c40b40f8098d926c71ccd0625368ae51b5f99ba1
Kartiki <67822105+kartiki001@users.noreply.github.com>
2025-02-20 16:58:47 +0530

    Time: 15 ms (43.28%) | Memory: 18.6 MB (11.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        return sorted(s) == sorted(t) #sorts strings in cache to check if the strings are same
+        return Counter(s) == Counter (t) #creates hashmap for both and check if they are same
